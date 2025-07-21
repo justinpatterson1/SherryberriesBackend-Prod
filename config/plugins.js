d6967852ec6e43c3e42a888719d90cb2bec03bbd
Justin Patterson <justin.patterson17@yahoo.com>
2025-07-20 22:05:37 -0400

    Added new mailgun domain to config

--- config/plugins.js
+++ config/plugins.js
@@ -55,8 +55,8 @@ module.exports = ({env}) => ({
         url:    env('MAILGUN_URL', 'https://api.mailgun.net'), // ← new name
       },
       settings: {
-        defaultFrom: 'sherryvanessanichols@gmail.com',
-        defaultReplyTo:'sherryvanessanichols@gmail.com',
+        defaultFrom: 'no-reply@mg.sherry-berries.com',
+        defaultReplyTo:'no-reply@mg.sherry-berries.com',
       },
     },
   },
